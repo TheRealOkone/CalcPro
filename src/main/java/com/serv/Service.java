7e5431858f2e3c1f76e24babc586be19bb83724e
O <tnoko@yandex.ru>
2021-02-16 13:21:02 +0300

    Added POST

--- src/main/java/com/serv/Service.java
+++ src/main/java/com/serv/Service.java
@@ -13,6 +13,12 @@ public class Service {
     public String getTestPage() {
         return "index.html";
     }
+    @CrossOrigin
+    @RequestMapping(value = "/home", method = RequestMethod.POST)
+    @ResponseBody
+    public String go(@RequestBody String req) {
 
+        return "bruh";
+    }
 
 }
