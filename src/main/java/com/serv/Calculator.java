706b8f1425793a3a20286069de58b2712fa00335
TheRealOkone <tnoko@yandex.ru>
2021-03-13 20:51:34 +0300

    Обновлен механизм вычислений

--- src/main/java/com/serv/Calculator.java
+++ src/main/java/com/serv/Calculator.java
@@ -2,114 +2,129 @@ package com.serv;
 
 import java.util.*;
 
+import static java.lang.Math.*;
+
 public class Calculator {
 
-    public static Double evaluate(String expression) {
-        List<String> strList = new ArrayList<>();
-        for (String listElement : expression.trim().split(" ")) {
-            strList.add(listElement);
-            strList.add(" ");
-        }
-        strList.remove(strList.size() - 1);
-        if (strList.indexOf("(") != -1) {
+    public static Double evaluate(ArrayList <String> strList) {
 
-            for (int i = strList.indexOf("(") + 1; i < strList.size() - 1; i++) {
-                String recursion = "";
-                if (strList.get(i).equals("(")) {
-                    for (int j = i; j < strList.lastIndexOf(")"); j++) {
-                        recursion += strList.get(j);
-                    }
-                    String test = expression.substring(expression.indexOf("("), expression.lastIndexOf(")") + 1);
-                    String testRecursion = String.valueOf(evaluate(recursion));
-                    expression = expression.replace(test, testRecursion);
-                    strList.removeAll(strList);
-                    for (String newElement : expression.trim().split(" ")) {
-                        strList.add(newElement);
-                        strList.add(" ");
-                    }
-                }
-                String recursion2 = "";
-                if (strList.get(i).equals(")")) {
-
-                    for (int j = strList.indexOf("(") + 1; j < strList.indexOf(")"); j++) {
-                        recursion2 += strList.get(j);
-                    }
-                    String test2 = expression.substring(expression.indexOf("("), expression.lastIndexOf(")") + 1);
-                    String testRecursion2 = String.valueOf(evaluate(recursion2));
-                    expression = expression.replace(test2, testRecursion2);
-                    for (String newElement : expression.trim().split(" ")) {
-                        strList.add(newElement);
-                        strList.add(" ");
-                    }
-                }
-            }
-        }
-        List<String> stringList2 = new ArrayList<>();
-        for (String element : expression.trim().split(" ")) {
-            stringList2.add(element);
+        if(strList.size() == 1){
+            return Double.valueOf(strList.get(0));
         }
 
+        while(strList.indexOf("(") == 0 && strList.indexOf(")") == strList.size()-1){
+            strList.remove(0);
+            strList.remove(strList.size()-1);
+        }
 
-        while (stringList2.size() != 0) {
-            Double result = 0d;
-            if (stringList2.indexOf("/") != -1) {
-                int index = stringList2.indexOf("/");
-                result = Double.valueOf(stringList2.get(index - 1)) / Double.valueOf(stringList2.get(index + 1));
-                stringList2.add(index - 1, String.valueOf(result));
-                stringList2.remove(index + 2);
-                stringList2.remove(index + 1);
-                stringList2.remove(index);
-            }
-            else if (stringList2.indexOf("*") != -1) {
-                int index = stringList2.indexOf("*");
-                result = Double.valueOf(stringList2.get(index - 1)) * Double.valueOf(stringList2.get(index + 1));
-                stringList2.add(index - 1, String.valueOf(result));
-                stringList2.remove(index + 2);
-                stringList2.remove(index + 1);
-                stringList2.remove(index);
-            }
-            else if (stringList2.indexOf("-") != -1) {
-                int index = stringList2.indexOf("-");
-                int lastIndex = stringList2.lastIndexOf("-");
-                if (index == 0) {
-                    result = 0.0 - Double.valueOf(stringList2.get(index + 1));
-                    stringList2.add(0, String.valueOf(result));
-                    stringList2.remove(2);
-                    stringList2.remove(1);
+        if(strList.contains("(")){
+            for(int i = 0; i < strList.size(); i++){
+                if(strList.get(i).equals("(")){
+                    int b;
+                    boolean succ = true;
+                    int testo = 0;
+                    ArrayList<String> str = new ArrayList<>();
+                    for(int j = i + 1; succ; j++){
+                        str.add(strList.get(j));
+                        b = j+1;
+                        if(strList.get(j + 1).equals("(")){
+                            testo++;
+                        }
+                        if(strList.get(j + 1).equals(")")){
+                            if(testo == 0) {
+                                succ = false;
+                            }
+                            else{
+                                testo--;
+                            }
+                        }
+
+                    }
+                    Double c = evaluate(str);
+                    strList.set(i,c.toString());
+                    while(!strList.get(i + 1).equals(")")){
+                        strList.remove(i + 1 );
+                    }
+                    strList.remove(i + 1 );
                 }
-                else if ((lastIndex-2>0) && (stringList2.get(lastIndex-2).equals("-"))){
-                    result = Double.valueOf(stringList2.get(lastIndex + 1)) + Double.valueOf(stringList2.get(lastIndex - 1));
-                    stringList2.add(lastIndex - 1, String.valueOf(result));
-                    stringList2.remove(lastIndex + 2);
-                    stringList2.remove(lastIndex + 1);
-                    stringList2.remove(lastIndex);
-                }
-                else {
-                    result = Double.valueOf(stringList2.get(index - 1)) - Double.valueOf(stringList2.get(index + 1));
-                    stringList2.add(index - 1, String.valueOf(result));
-                    stringList2.remove(index + 2);
-                    stringList2.remove(index + 1);
-                    stringList2.remove(index);
-                }
-            }
-            else if (stringList2.indexOf("+") != -1) {
-                int index = stringList2.indexOf("+");
-                result = Double.valueOf(stringList2.get(index - 1)) + Double.valueOf(stringList2.get(index + 1));
-                stringList2.add(index - 1, String.valueOf(result));
-                stringList2.remove(index + 2);
-                stringList2.remove(index + 1);
-                stringList2.remove(index);
-            }
-            if ((stringList2.indexOf("*") == -1) && (stringList2.indexOf("/") == -1) && (stringList2.indexOf("+") == -1) && (stringList2.indexOf("-") == -1)) {
-                return result;
             }
         }
-        return Double.valueOf(stringList2.get(0));
+
+        if(!strList.contains("(")){
+            if(strList.contains("+")){
+                List<String> subList = new ArrayList<>();
+                for(int i = 0; i < strList.indexOf("+"); i++){
+                    subList.add(strList.get(i));
+                }
+                List<String> subList2 = new ArrayList<>();
+                for(int i = strList.indexOf("+") + 1; i < strList.size(); i++){
+                    subList2.add(strList.get(i));
+                }
+                 return goCalc(subList, "+", subList2);
+
+            }
+            if(strList.contains("-")){
+                List<String> subList = new ArrayList<>();
+                for(int i = 0; i < strList.indexOf("-"); i++){
+                    subList.add(strList.get(i));
+                }
+                List<String> subList2 = new ArrayList<>();
+                for(int i = strList.indexOf("-") + 1; i < strList.size(); i++){
+                    subList2.add(strList.get(i));
+                }
+                return goCalc(subList, "-", subList2);
+
+            }
+            if(strList.contains("*")){
+                List<String> subList = new ArrayList<>();
+                for(int i = 0; i < strList.indexOf("*"); i++){
+                    subList.add(strList.get(i));
+                }
+                List<String> subList2 = new ArrayList<>();
+                for(int i = strList.indexOf("*") + 1; i < strList.size(); i++){
+                    subList2.add(strList.get(i));
+                }
+                return goCalc(subList, "*", subList2);
+
+            }
+            if(strList.contains("/")){
+                List<String> subList = new ArrayList<>();
+                for(int i = 0; i < strList.indexOf("/"); i++){
+                    subList.add(strList.get(i));
+                }
+                List<String> subList2 = new ArrayList<>();
+                for(int i = strList.indexOf("/") + 1; i < strList.size(); i++){
+                    subList2.add(strList.get(i));
+                }
+                return goCalc(subList, "/", subList2);
+
+            }
+        }
+        return 0.0;
+    }
+
+    private static Double goCalc(List<String> subList, String s, List<String> subList2) {
+        switch(s)
+        {
+            case "+":
+                return evaluate((ArrayList<String>) subList) + evaluate((ArrayList<String>) subList2);
+
+            case "-":
+                return evaluate((ArrayList<String>) subList) - evaluate((ArrayList<String>) subList2);
+
+            case "*":
+                return evaluate((ArrayList<String>) subList) * evaluate((ArrayList<String>) subList2);
+
+            case "/":
+                return evaluate((ArrayList<String>) subList) / evaluate((ArrayList<String>) subList2);
+        }
+        return 0.0;
     }
 
 
+
     public double calc(String req){
-        return evaluate(req);
+        return evaluate(new ArrayList<>(Arrays.asList(req.trim().split(" "))));
     }
 
 }
