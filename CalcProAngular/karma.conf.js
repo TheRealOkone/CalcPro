c5c05ac6763e02c2994c3b988e841154758efaad
Dmitry Egorov (PC) <mitya2001egorov@gmail.com>
2021-05-12 21:17:53 +0300

    Пофиксил имена

--- CalcProAngular/karma.conf.js
+++ CalcProAngular/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/CalcProAngular'),
+      dir: require('path').join(__dirname, './coverage/calcproangular'),
       subdir: '.',
       reporters: [
         { type: 'html' },
