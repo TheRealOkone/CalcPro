99e099650a65739f04a4b38a6c8c898a4aa37f44
TheRealOkone <tnoko@yandex.ru>
2021-05-25 20:20:27 +0300

    report mod5

--- CalcPro/src/main/java/com/serv/SimpleCORSFilter.java
+++ CalcPro/src/main/java/com/serv/SimpleCORSFilter.java
@@ -33,7 +33,7 @@ public class SimpleCORSFilter implements Filter {
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Methods", "*");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("", "*");
+        response.setHeader("Access-Control-Allow-Headers", "*");
         chain.doFilter(req, res);
     }
 
---
+++ CalcPro/target/site/apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"com.serv","l":"Calculator"},{"p":"com.serv","l":"RunApp"},{"p":"com.serv","l":"Service"},{"p":"com.serv","l":"SimpleCORSFilter"},{"p":"com.serv","l":"WebConfig"}];updateSearchResults();
